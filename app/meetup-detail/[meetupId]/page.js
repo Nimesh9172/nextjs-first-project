090a686fec3ed39aea7ab1d3710d2f599ca200e3
Nimesh9172 <nimeshdj123@gmail.com>
2023-08-20 22:26:52 +0530

    first

---
+++ app/meetup-detail/[meetupId]/page.js
@@ -0,0 +1,8 @@
+import MeetupDetail from "@/app/components/meetups/MeetupDetail"
+
+const MeetupDetailPage = () => {
+    return (
+       <MeetupDetail image="https://commonwealthmagazine.org/wp-content/uploads/2016/05/5561058399_2adf917870_k.jpg" title="A Second meetup" description="This is a second meetup"/>
+    )
+}
+export default MeetupDetailPage
---
+++ app/new-meetup/page.js
@@ -0,0 +1,14 @@
+
+'use client'
+import NewMeetupForm from "../components/meetups/NewMeetupForm"
+
+const NewMeetupPage = () => {
+
+    const addMeetupHandler = (meetupData) => {
+        console.log(meetupData)
+    }
+
+    return <NewMeetupForm onAddMeetup={addMeetupHandler}/> 
+}
+
+export default NewMeetupPage
