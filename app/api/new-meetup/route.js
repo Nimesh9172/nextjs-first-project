dc94a51e685548692a2621da9e1a9d244b1204f9
Nimesh9172 <nimeshdj123@gmail.com>
2023-08-23 23:20:48 +0530

    final indent

--- app/api/new-meetup/route.js
+++ app/api/new-meetup/route.js
@@ -5,25 +5,24 @@ const uri =
   "mongodb+srv://nimesh:5UtJ1qb93zf9iIMC@cluster0.01ne6v4.mongodb.net";
 
 export async function POST(req, res) {
+  const body = await req.json();
 
-  const body = await req.json()
+  // // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+  const client = new MongoClient(uri);
 
-    // // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-    const client = new MongoClient(uri);
-   
-      try {
-        const database = client.db("first_db");
-        const myColl = database.collection("meetup");
-        const result = await myColl.insertOne(body);
-        console.log(`A document was inserted with the _id: ${result.insertedId}`);
-        return NextResponse.json({ message:"Please enter title" }, { status: 201 });
-      }catch (err){
-        console.log(err)
-      } finally {
-        // Ensures that the client will close when you finish/error
-        await client.close();
-      }
-    
-  
-
+  try {
+    const database = client.db("first_db");
+    const myColl = database.collection("meetup");
+    const result = await myColl.insertOne(body);
+    console.log(`A document was inserted with the _id: ${result.insertedId}`);
+    return NextResponse.json(
+      { message: "Please enter title" },
+      { status: 201 }
+    );
+  } catch (err) {
+    console.log(err);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
 }
