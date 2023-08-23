71af2f39b5b9f787d3e7680948e67d6fab9c0e6c
Nimesh9172 <nimeshdj123@gmail.com>
2023-08-23 23:20:20 +0530

    succesfully inserted

--- app/api/new-meetup/route.js
+++ app/api/new-meetup/route.js
@@ -2,32 +2,28 @@ import { MongoClient, ServerApiVersion } from "mongodb";
 import { NextResponse } from "next/server";
 
 const uri =
-  "mongodb+srv://nimesh:61oWf7qUduCI7w4x@cluster0.01ne6v4.mongodb.net/first_db/?retryWrites=true&w=majority";
+  "mongodb+srv://nimesh:5UtJ1qb93zf9iIMC@cluster0.01ne6v4.mongodb.net";
 
 export async function POST(req, res) {
-  const body = await req.json();
 
-  // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-  const client = new MongoClient(uri, {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    },
-  });
-  async function run() {
-    try {
-      // Connect the client to the server	(optional starting in v4.7)
-      await client.connect();
-      // Send a ping to confirm a successful connection
-      const client = await client.db("first_db");
-      console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-      // Ensures that the client will close when you finish/error
-      await client.close();
-    }
-  }
-  run().catch(console.dir);
+  const body = await req.json()
 
-  return NextResponse.json({ message: "Please enter title" }, { status: 201 });
+    // // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+    const client = new MongoClient(uri);
+   
+      try {
+        const database = client.db("first_db");
+        const myColl = database.collection("meetup");
+        const result = await myColl.insertOne(body);
+        console.log(`A document was inserted with the _id: ${result.insertedId}`);
+        return NextResponse.json({ message:"Please enter title" }, { status: 201 });
+      }catch (err){
+        console.log(err)
+      } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+      }
+    
+  
+
 }
