da21c843cbe04245537996095630f7c884ff7ce3
nimesh9172 <nimeshdj123@gmail.com>
2023-08-23 17:56:40 +0530

    ch

--- app/api/new-meetup/route.js
+++ app/api/new-meetup/route.js
@@ -20,10 +20,8 @@ export async function POST(req, res) {
       // Connect the client to the server	(optional starting in v4.7)
       await client.connect();
       // Send a ping to confirm a successful connection
-      await client.db("admin").command({ ping: 1 });
-      console.log(
-        "Pinged your deployment. You successfully connected to MongoDB!"
-      );
+      const client = await client.db("first_db");
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
       // Ensures that the client will close when you finish/error
       await client.close();
